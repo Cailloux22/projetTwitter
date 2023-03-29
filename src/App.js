ddc2f58911cc2957088dfa62202928f89fa0498d
Cailloux22 <98032839+Cailloux22@users.noreply.github.com>
2023-03-29 11:44:23 +0200

    rooter modul

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
+  </BrowserRouter>;
   return <div>helloooo</div>;
 }
 
