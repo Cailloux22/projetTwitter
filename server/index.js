090026201ede3ba64d38608b10a5e1773e279948
Cailloux22 <98032839+Cailloux22@users.noreply.github.com>
2023-04-08 13:08:11 +0200

    get timeline by rendom user id

--- server/index.js
+++ server/index.js
@@ -9,11 +9,63 @@ server.listen(3001, () => {
   console.log("listening on :3001");
 });
 
-app.get("/api/v1/store", async (req, res) => {
+app.get("/api/v1/getAccountIdByName", async (req, res) => {
  
-  console.log("oyooow");
+  console.log("getAccountIdByName");
   fetchUrl(
     "https://api.twitter.com/2/users/by/username/elonmusk",
+    {
+      headers: {
+        authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAHMflgEAAAAALPWElwpK3pSg8lAJJAHz4P%2Fz4Pk%3DYAzTthQCsIbnMMTCu7bYMeRcrMZizTAuAgfG5KLJci4l5BwVUq'
+          
+      },
+    },
+    (err, meta, body) => {
+      if (err) {
+        console.log("error", err);
+        return false;
+      }
+       res.json({ status:meta.status, apiTwitter:JSON.parse( body.toString())})
+      console.log("body", body.toString());
+      console.log("meta", meta);
+    }
+  );
+ 
+});
+
+
+app.get("/api/v1/getAccountById/:id", async (req, res) => {
+ 
+  console.log("getAccountById",req.params.id);
+  fetchUrl(
+    "https://api.twitter.com/2/users/"+req.params.id,
+    {
+      headers: {
+        authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAHMflgEAAAAALPWElwpK3pSg8lAJJAHz4P%2Fz4Pk%3DYAzTthQCsIbnMMTCu7bYMeRcrMZizTAuAgfG5KLJci4l5BwVUq'
+          
+      },
+    },
+    (err, meta, body) => {
+      if (err) {
+        console.log("error", err);
+        return false;
+      }
+       res.json({ status:meta.status, apiTwitter:JSON.parse( body.toString())})
+      console.log("body", body.toString());
+      console.log("meta", meta);
+    }
+  );
+ 
+});
+
+
+// https://api.twitter.com/2/users/:id/tweets
+
+app.get("/api/v1/getTimelineByUserId/:id", async (req, res) => {
+ 
+  console.log("getAccountById",req.params.id);
+  fetchUrl(
+    "https://api.twitter.com/2/users/"+req.params.id+"/tweets",
     {
       headers: {
         authorization: 'Bearer AAAAAAAAAAAAAAAAAAAAAHMflgEAAAAALPWElwpK3pSg8lAJJAHz4P%2Fz4Pk%3DYAzTthQCsIbnMMTCu7bYMeRcrMZizTAuAgfG5KLJci4l5BwVUq'
--- src/components/tweets.js
+++ src/components/tweets.js
@@ -1,16 +1,54 @@
 import React, { useEffect, useState } from "react";
 import uuid from "react-uuid";
 import Tweet from "./tweet";
-
+import { AiFillTwitterCircle } from "react-icons/ai";
+import "../App.css";
 const Tweets = () => {
   const [tweets, setTweets] = useState([1, 2, 1, 11, 1, 1, 1, 1, 1]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentId, setCurrentId] = useState();
+  const [initialRender, setInitialRender] = useState(true);
+  useEffect(() => {
+    fetchID();
+  }, []);
+
+  //  FETCH
+  const fetchID = async (recid = false) => {
+    if (!recid) {
+      var id = Math.floor(Math.random() * 10000);
+    } else {
+      id = recid;
+    }
+    console.log(id);
+    await fetch("http://localhost:3001/api/v1/getAccountById/" + id, {})
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        if (result.apiTwitter.errors) {
+          setTimeout(() => {
+            fetchID(id + 1);
+          }, 500);
+        } else {
+          setCurrentId(id);
+          setIsLoading(false);
+        }
+      });
+  };
 
   useEffect(() => {
-    fetchTweets();
-  }, []);
-  //  FETCH
+    if (initialRender) {
+      setInitialRender(false);
+    } else {
+      fetchTweets();
+    }
+  }, [currentId]);
+
   const fetchTweets = async () => {
-    await fetch("http://localhost:3001/api/v1/store", {})
+    console.log("gooo !", currentId);
+    await fetch(
+      "http://localhost:3001/api/v1/getTimelineByUserId/" + currentId,
+      {}
+    )
       .then((res) => res.json())
       .then((result) => {
         console.log(result);
@@ -19,6 +57,16 @@ const Tweets = () => {
 
   return (
     <>
+      {isLoading && (
+        <div className="fixed bg-gray-500 opacity-50 w-screen h-screen z-1111111">
+          <div className="bg-white fixed top-1/2 left-1/2 -translate-x-2/4 p-6 -translate-y-2/4 flex flex-col items-center">
+            <div className="w-full h-full flex items-center justify-center rounded-md">
+              <AiFillTwitterCircle className="loader text-cyan-500" size={48} />
+            </div>
+          </div>
+        </div>
+      )}
+
       <h1>les tweets</h1>
       {tweets.map(() => (
         <Tweet key={uuid()} />
