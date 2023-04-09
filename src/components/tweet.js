c9dddd32d279c3d24c7e163af1e9e144f9aa47c0
Cailloux22 <98032839+Cailloux22@users.noreply.github.com>
2023-04-09 18:59:04 +0200

    wouaw

--- src/components/tweet.js
+++ src/components/tweet.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import ReactPlayer from "react-player";
 
 const Tweet = ({tweet}) => {
     
     
     return (
         <>
-            <div className="mx-auto w-3/4 border-b border-r border-l border-gray-400 p-2">{tweet.text}</div>
+        
+            <div className="mx-auto w-3/4 border-t border-r border-l border-gray-300 p-2">{tweet.text}</div>
         </>
     )
 }
